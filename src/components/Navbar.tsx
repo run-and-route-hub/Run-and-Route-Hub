ecc75b01af1f3ef415ad166b8f4fe4653d95520b
Jacob-Hatanaka <jacobth@hawaii.edu>
2025-11-10 11:31:42 -1000

    remove stuff from database and navbar
    does not remove functions related to stuff in pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,18 +16,18 @@ const NavBar: React.FC = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Next.js Application Template</Navbar.Brand>
+        <Navbar.Brand href="/">Run & Route Hub</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
             {currentUser
               ? [
-                  <Nav.Link id="add-stuff-nav" href="/add" key="add" active={pathName === '/add'}>
+                  /* <Nav.Link id="add-stuff-nav" href="/add" key="add" active={pathName === '/add'}>
                     Add Stuff
                   </Nav.Link>,
                   <Nav.Link id="list-stuff-nav" href="/list" key="list" active={pathName === '/list'}>
                     List Stuff
-                  </Nav.Link>,
+                  </Nav.Link>, */
                 ]
               : ''}
             {currentUser && role === 'ADMIN' ? (
