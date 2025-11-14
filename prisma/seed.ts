d5807eea13355201af46f061a9ffd9bfa52856ab
Preston Woo <ppw@hawaii.edu>
2025-11-14 13:04:47 -1000

    build change

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,3 @@
-/*
 import { PrismaClient, Role, Condition } from '@prisma/client';
 import { hash } from 'bcrypt';
 import * as config from '../config/settings.development.json';
@@ -9,8 +8,8 @@ async function main() {
   console.log('Seeding the database');
   const password = await hash('changeme', 10);
   config.defaultAccounts.forEach(async (account) => {
-    const role = account.role as Role  Role.USER;
-    console.log(  Creating user: ${account.email} with role: ${role});
+    const role = account.role as Role || Role.USER;
+    console.log(`  Creating user: ${account.email} with role: ${role}`);
     await prisma.user.upsert({
       where: { email: account.email },
       update: {},
@@ -20,11 +19,11 @@ async function main() {
         role,
       },
     });
-    // console.log(  Created user: ${user.email} with role: ${user.role});
+    // console.log(`  Created user: ${user.email} with role: ${user.role}`);
   });
   for (const data of config.defaultData) {
-    const condition = data.condition as Condition  Condition.good;
-    console.log(  Adding stuff: ${JSON.stringify(data)});
+    const condition = data.condition as Condition || Condition.good;
+    console.log(`  Adding stuff: ${JSON.stringify(data)}`);
     // eslint-disable-next-line no-await-in-loop
     await prisma.stuff.upsert({
       where: { id: config.defaultData.indexOf(data) + 1 },
@@ -44,45 +43,4 @@ main()
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
-  });
-*/
-
-import { PrismaClient, Role } from '@prisma/client';
-import { hash } from 'bcrypt';
-import * as config from '../config/settings.development.json';
-
-const prisma = new PrismaClient();
-
-async function main() {
-  console.log('Seeding database...');
-
-  for (const account of config.defaultAccounts) {
-    console.log(`➡️ Creating user: ${account.email}`);
-
-    // eslint-disable-next-line no-await-in-loop
-    const hashedPassword = await hash(account.password, 10);
-
-    // eslint-disable-next-line no-await-in-loop
-    await prisma.user.upsert({
-      where: { email: account.email },
-      update: {},
-      create: {
-        email: account.email,
-        password: hashedPassword,
-        role: account.role ? (account.role as Role) : Role.USER,
-      },
-    });
-  }
-
-  console.log('Seeding complete!');
-}
-
-main()
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
   });
