eb824ef337afb2eb32feb617cd16b06f57354312
albertlks <albertlscales@gmail.com>
2025-12-01 16:52:54 -1000

    updated playwright tests

--- tests/routes-page.spec.ts
+++ tests/routes-page.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+const baseURL = process.env.TEST_URL || 'http://localhost:3000';
+
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/routes');
+  await page.goto(`${baseURL}/routes`);
   await expect(
     page.getByRole('heading', { name: 'Routes' }),
   ).toBeVisible();
