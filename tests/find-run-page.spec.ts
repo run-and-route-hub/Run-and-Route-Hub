7cc20f4f917a57133b62fc082813aedb5f035c7c
albertlks <albertlscales@gmail.com>
2025-12-01 19:30:59 -1000

    update

--- tests/find-run-page.spec.ts
+++ tests/find-run-page.spec.ts
@@ -1,9 +1,11 @@
 import { test } from '@playwright/test';
+import TEST_URL from './setup';
 
 test('test', async ({ page }) => {
-  await page.goto(`${process.env.TEST_URL}/find-run`);
-  await page.getByLabel('Difficulty Level:').selectOption('Easy');
-  await page.getByLabel('Minimum Distance:Any1 mile2').selectOption('1 mile');
-  await page.getByLabel('Preferred Pace:').selectOption('Slow');
+  await page.goto(`${TEST_URL}/find-run`, { waitUntil: 'networkidle' });
+  await page.locator('main').waitFor({ state: 'visible', timeout: 10000 });
+  await page.locator('#difficulty').selectOption({ label: 'Easy' });
+  await page.locator('#minDistance').selectOption({ label: '1 mile' });
+  await page.locator('#pace').selectOption({ label: 'Slow' });
   await page.getByRole('heading', { name: 'Find Run' }).click();
 });
