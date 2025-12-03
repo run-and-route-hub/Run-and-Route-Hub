43171c1a2e7d6adde9e7e150f4a6fa1108dcd578
Jacob-Hatanaka <jacobth@hawaii.edu>
2025-12-03 11:41:03 -1000

    include start and loop in polyline

--- src/components/AddRouteForm.tsx
+++ src/components/AddRouteForm.tsx
@@ -245,7 +245,8 @@ const AddRouteForm: React.FC = () => {
               ))}
               {route.end && <Marker position={route.end} label="E" title="End" />}
               <Polyline
-                path={route.path.map((point: { lat: number; lng: number }) => ({ lat: point.lat, lng: point.lng }))}
+                // eslint-disable-next-line max-len
+                path={[{ lat: route.start.lat, lng: route.start.lng }, ...route.path.map((point: { lat: number; lng: number }) => ({ lat: point.lat, lng: point.lng })), ...(route.loop ? [{ lat: route.end.lat, lng: route.end.lng }] : [])]}
                 options={{
                   strokeColor: '#1d4ed8',
                   strokeWeight: 5,
