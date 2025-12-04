3535797048f798598a47c66755143afbb8afcce8
albertlks <albertlscales@gmail.com>
2025-12-03 14:13:54 -1000

    Update setup.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,2 +1,5 @@
-const TEST_URL = process.env.TEST_URL || 'http://localhost:3000';
+const TEST_URL = process.env.CI
+  ? process.env.TEST_URL || 'https://your-vercel-deployment.vercel.app'
+  : 'http://localhost:3000';
+
 export default TEST_URL;
