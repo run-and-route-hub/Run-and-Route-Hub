46a3944ce02e39bbc220180ad2c543f5ed26ce8e
albertlks <albertlscales@gmail.com>
2025-12-11 14:11:16 -1000

    Update add-run-page.spec.ts

--- tests/add-run-page.spec.ts
+++ tests/add-run-page.spec.ts
@@ -9,6 +9,6 @@ test('test', async ({ page }) => {
   await page.locator('input[name="password"]').fill('changeme');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.waitForURL(`${TEST_URL}`, { timeout: 10000 });
-  await page.goto(`${TEST_URL}/add-run`);
+  await page.goto(`${TEST_URL}/add-run`, { waitUntil: 'domcontentloaded' });
   await page.locator('h1', { hasText: 'Add Route' }).waitFor({ timeout: 15000 });
 });
